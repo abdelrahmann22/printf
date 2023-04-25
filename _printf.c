910fe91d7c442fd228cef850a2fa544231496a6a
abdelrahmann22 <boudyali22@gmail.com>
2023-04-25 22:48:49 +0200

    Check 5

--- _printf.c
+++ _printf.c
@@ -1,6 +1,51 @@
 #include "main.h"
 
 /**
+ * print_md - prints anything
+ * @format: the format to print
+ * @specifiers: the specifiers to print
+ * @args: the arguments to print
+ * 
+ * Return: number of char printed
+*/
+int print_md(const char *format, specifier_t *specifiers, va_list args)
+{
+	char a;
+	int count = 0, i = 0, j = 0;
+
+	a = format[i];
+	while (a != '\0')
+	{
+		if (a == '%')
+		{
+			j = 0;
+			i++;
+			a = format[i];
+			while (specifiers[j].type != NULL && a != *(specifiers[j].type) )
+				j++;
+			if (specifiers[j].type != NULL)
+			{
+				count += specifiers[j].f(args);
+			}
+			else
+			{
+				if (a == '\0')
+					return (-1);
+				if (a != '%')
+					count += _putchar('%');
+				count += _putchar(a);
+			}
+		}
+		else
+			count += _putchar(a);
+		
+		i++;
+		a = format[i];
+	}
+	return (count);
+}
+
+/**
  * _printf - prints anything
  * @format: the format to print
  * 
@@ -37,49 +82,4 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (i);
 
-}
-
-/**
- * print_md - prints anything
- * @format: the format to print
- * @specifiers: the specifiers to print
- * @args: the arguments to print
- * 
- * Return: number of char printed
-*/
-int print_md(const char *format, specifier_t *specifiers, va_list args)
-{
-	char a;
-	int count = 0, i = 0, j = 0;
-
-	a = format[i];
-	while (a != '\0')
-	{
-		if (a == '%')
-		{
-			j = 0;
-			i++;
-			a = format[i];
-			while (specifiers[j].type != NULL && a != *(specifiers[j].type) )
-				j++;
-			if (specifiers[j].type != NULL)
-			{
-				count += specifiers[j].f(args);
-			}
-			else
-			{
-				if (a == '\0')
-					return (-1);
-				if (a != '%')
-					count += _putchar('%');
-				count += _putchar(format[i]);
-			}
-		}
-		else
-			count += _putchar(format[i]);
-		
-		i++;
-		a = format[i];
-	}
-	return (count);
 }
