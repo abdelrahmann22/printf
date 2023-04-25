6cc859eb0372124945dd367b8467c6d8417743cf
abdelrahmann22 <boudyali22@gmail.com>
2023-04-26 01:17:10 +0200

    Check

--- _putchar.c
+++ _putchar.c
@@ -8,5 +8,18 @@
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buff[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, buff, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buff[i] = c;
+		i++;
+	}
+	return (1);
 }
