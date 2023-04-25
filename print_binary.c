807144939beabd1ee55e7174584070113d97dcf5
abdelrahmann22 <boudyali22@gmail.com>
2023-04-25 23:23:33 +0200

    Check 1

---
+++ print_binary.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a binary number
+ * @args: the binary number to print
+ * 
+ * Return: number of char printed
+*/
+int print_binary(va_list args)
+{
+	unsigned int i = 0, count = 0, binary, arr[32], num;
+	
+	num = va_arg(args, unsigned int);
+
+	if (num < 1)
+	{
+		_putchar('48');
+		count++;
+		return (count);
+	}
+	else
+	{
+		while (num > 0)
+		{
+			binary = num % 2;
+			arr[i] = binary;
+			num /= 2;
+			i++;
+		}
+		i = count - 1;
+		while (i > 0)
+		{
+			_putchar(arr[i] + '0');
+			i--;
+		}
+		_putchar(arr[i] + '0');
+	}
+	return (count);
+}
